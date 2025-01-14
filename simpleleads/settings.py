e9cdb0298d678c0047e0f2475faad11ac8531235
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-01-13 22:20:51 -0600

    fixes

--- simpleleads/settings.py
+++ simpleleads/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-7co&%h#a7-_l#jupblq*2z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split()
 
 # Application definition
